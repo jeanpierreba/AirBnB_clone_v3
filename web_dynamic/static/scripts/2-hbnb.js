92187a0ac12f6323b0dd6a23855acb0ba3777174
Edheramirez <edher.ramirez@outlook.es>
2021-09-25 21:48:21 -0500

    test h4 don´t print

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -3,20 +3,21 @@ $(document).ready(init);
 function init () {
   const amenity_ids = {};
   $('input[type=checkbox]').change(function () {
-    if ($(this).prop('checked')) {
-      amenity_ids[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else if (!$(this).prop('checked')) {
-      delete amenity_ids[$(this).attr('data-id')];
+    if ($(this).is(':checked')) {
+      amenity_ids[$(this).data('id')] = $(this).data('name');
+    } else if (!$(this).is('(:checked)')) {
+      delete amenity_ids[$(this).data('id')];
     }
-    if (Objects.keys(amenity_ids).length === 0) {
+    if (Object.values(amenity_ids).length === 0) {
       $('.amenities h4').html('&nbsp');
     } else {
-      $('.amenities h4').text(Object.values(amenity_ids).sort().join(', '));
+      const names = Object.values(amenity_ids);
+      $('.amenities h4').html(names.sort().join(', '));
     }
   });
+  apiStatus();
 }
 
-apiStatus();
 function apiStatus () {
   $.ajax('http://0.0.0.0:5001/api/v1/status/').done(function (data) {
     if (data.status === 'OK') {
